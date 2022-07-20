9f56f5f305179cbebf7268f89842e346cc50d17d
Hadrien-Rudich <94840435+Hadrien-Rudich@users.noreply.github.com>
2022-07-20 10:44:49 +0200

    react-model boilerplate implemented

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+// == Import
+import './styles.css';
+
+// == Composant
+function App() {
+  return (
+    <div className="app">
+      <h1>Composant : App</h1>
+    </div>
+  );
+}
+
+// == Export
+export default App;
