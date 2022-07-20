e362f1ae12b8941bf0847f77870174fa222ff820
Hadrien-Rudich <94840435+Hadrien-Rudich@users.noreply.github.com>
2022-07-20 17:16:37 +0200

    controlled input fields implemented

--- src/middlewares/userMiddleware.js
+++ src/middlewares/userMiddleware.js
@@ -1,8 +1,15 @@
 /* eslint-disable no-unused-vars */
 import axios from 'axios';
 
+import { SUBMIT_LOGIN, submitLoginSuccess } from 'src/actions/user';
+
 const userMiddleware = (store) => (next) => (action) => {
   switch (action.type) {
+    case SUBMIT_LOGIN: {
+      store.dispatch(submitLoginSuccess());
+      next(action);
+      break;
+    }
     default:
       next(action);
   }
