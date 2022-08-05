1b83a45889a19e64461a9a8f3963d629c9f8fe86
Juan Quiros <jfquiros35@gmail.com>
2022-08-05 11:07:52 +0200

    ajout routes page 404

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -24,6 +24,7 @@ import Character from 'src/components/Character/Details';
 import CharacterEquipment from 'src/components/Character/Details/Equipment';
 import CharacterProficiencies from 'src/components/Character/Details/Proficiencies';
 import CharacterHistory from 'src/components/Character/Details/History';
+import NotFound from 'src/components/404';
 
 import './styles.css';
 
@@ -129,6 +130,7 @@ function App() {
           <Route path="/character/equipment" element={<CharacterEquipment />} />
           <Route path="/character/proficiencies" element={<CharacterProficiencies />} />
           <Route path="/character/history" element={<CharacterHistory />} />
+          <Route path="/404" element={<NotFound />} />
 
         </Routes>
         <Footer />
